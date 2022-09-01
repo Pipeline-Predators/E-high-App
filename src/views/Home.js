b0b0798d6b82be7d5dd9cbc8c7f2875f34265c5e
Akanlegum John <akanlegumjohn2@gmail.com>
2022-09-01 01:14:14 +0000

    Added article components for the take quiz, Review Past questions and take challenge sections

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,26 +1,37 @@
-import ArticleAlternateComponent from 'components/Articles/ArticleAlternateComponent'
-import ArticleComponent from 'components/Articles/ArticleComponent'
-import React from 'react'
-import { Row, Col, Button } from 'reactstrap'
-import Predator from '../assets/logo/logo.png'
+import ArticleAlternateComponent from "components/Articles/ArticleAlternateComponent";
+import ArticleComponent from "components/Articles/ArticleComponent";
+import React from "react";
+import { Row, Col, Button } from "reactstrap";
+import Predator from "../assets/logo/logo.png";
+import imageOne from "../assets/img/education_two.png";
+import imageTwo from "../assets/img/education_three.png";
+import imageThree from "../assets/img/education_one.png";
+import { useHistory } from "react-router-dom";
 
 const Home = (props) => {
+  const history = useHistory();
   return (
     <Row className="mx-0">
       <Col className="px-0" lg={12} xs={12}>
-        <div className='header-bg d-none d-lg-flex'></div>
-        <div className='header-bg-mobile d-lg-none d-sm-flex'></div>
-        <section className='header-content d-flex justify-content-center'>
-          <div className='w-50 d-none d-lg-flex align-items-center'>
+        <div className="header-bg d-none d-lg-flex"></div>
+        <div className="header-bg-mobile d-lg-none d-sm-flex"></div>
+        <section className="header-content d-flex justify-content-center">
+          <div className="w-50 d-none d-lg-flex align-items-center">
             <div className="header-hexagon mr-5">
               <img src={Predator} alt="company logo" />
             </div>
-            <div className>
-              <h2>Lorem ipsum dolor sit amet.</h2>
-              <Button>Get Started</Button>
+            <div>
+              <h2>Welcome to the E-high testing platform</h2>
+              <h5>
+                Test your might against some of the toughtest exams in the
+                country
+              </h5>
+              <Button onClick={() => history.push("../subject")}>
+                Get Started
+              </Button>
             </div>
           </div>
-          <div className='w-50 d-lg-none d-sm-flex flex-column align-items-center'>
+          <div className="w-50 d-lg-none d-sm-flex flex-column align-items-center">
             <div className="header-hexagon d-flex justify-content-center w-100">
               <img src={Predator} alt="company logo" />
             </div>
@@ -32,23 +43,34 @@ const Home = (props) => {
         </section>
       </Col>
       <Col lg={12} xs={12}>
-        <ArticleComponent/>
+        <ArticleAlternateComponent
+          cardImage={imageTwo}
+          title="Review Past Questions"
+          url="/subject"
+        />
         <hr />
       </Col>
       <Col lg={12} xs={12}>
-        <ArticleAlternateComponent/>
+        <ArticleAlternateComponent />
         <hr />
       </Col>
       <Col lg={12} xs={12}>
-        <ArticleComponent/>
+        <ArticleAlternateComponent
+          cardImage={imageTwo}
+          title="Review Past Questions"
+          url="/subject"
+        />
         <hr />
       </Col>
       <Col lg={12} xs={12}>
-        <ArticleAlternateComponent/>
-        <hr />
+        <ArticleComponent
+          cardImage={imageThree}
+          title="Take Challenge"
+          url="/subject"
+        />
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
