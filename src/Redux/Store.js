9e8e20900af60e1f5338461167acbe7bbfc8a94b
KwekuKweku Ansah <kweku4sta@gmail.com>
2022-09-01 02:19:53 +0000

    Added the redux folder with four components in it. the store,studentslice,subjectslice and the takequizslice

---
+++ src/Redux/Store.js
@@ -0,0 +1,53 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import subjectReducer from "./SubjectSlice";
+import studentReducer from "./StudentSlice";
+import takeQuizReducer from "./TakeQuizSlice";
+
+const subjectConfig = {
+  key: "subject",
+  version: 1,
+  storage: storage,
+  whitelist: ["value"],
+};
+
+const studentConfig = {
+  key: "student",
+  version: 1,
+  storage: storage,
+  whitelist: ["token"],
+};
+
+const takeQuizConfig = {
+  key: "takeQuiz",
+  version: 1,
+  storage,
+};
+
+const store = configureStore({
+  reducer: {
+    subject: persistReducer(subjectConfig, subjectReducer),
+    student: persistReducer(studentConfig, studentReducer),
+    takeQuiz: persistReducer(takeQuizConfig, takeQuizReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+let persistor = persistStore(store);
+
+export { store, persistor };
