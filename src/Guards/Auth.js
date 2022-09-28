496cd86c99d36536de6a7a289b6fdcd583a820d0
Kofi-Noble <asensoboadi814@gmail.com>
2022-09-28 03:03:20 +0000

    Footer chs

--- src/Guards/Auth.js
+++ src/Guards/Auth.js
@@ -25,7 +25,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ student, login, logout }}>
+    <AuthContext.Provider value={{student, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
