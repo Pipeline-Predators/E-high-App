8d4d2b739392c04ed0c8a0ccf63f08b02c9ea7d9
Kofi-Noble <asensoboadi814@gmail.com>
2022-10-03 23:39:26 +0000

    updated the css code

--- src/views/TakeChallenge.js
+++ src/views/TakeChallenge.js
@@ -1,7 +1,6 @@
 import ChallengeCard from "components/ChallengeCard/ChallengeCard";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import TakeChallengeRules from "components/ChallengeCard/TakeChallengeRules";
-
 
 const TakeChallenge = () => {
   const [body, setBody] = useState(
